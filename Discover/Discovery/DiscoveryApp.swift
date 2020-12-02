75d16e1825d1c5283387a4e4f71663d39559840c
CoreyBurgin <corey.burgin22@gmail.com>
2020-12-02 13:07:57 -0500

    Initial Project Setup

---
+++ Discover/Discovery/DiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoveryApp.swift
+//  Discovery
+//
+//  Created by Corey Burgin on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct DiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
